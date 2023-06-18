55176fefcfdbbbc73eb752a31bb22a1ce012dcf8
Arthur Almeida <arthuralmeida.office.dev@gmail.com>
2023-06-18 17:03:23 -0300

    /feat route module

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
-#[macro_use] extern crate rocket;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, user!"
-}
-
-#[get("/")]
-fn admin_index() -> &'static str {
-    "Hello, admin!"
-}
-
-// #[get("/coolroute/<>")]
+#[macro_use]
+extern crate rocket;
+mod routes;
+use routes::public;
+use routes::protected;
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .mount("/", routes![index])
-    .mount("/admin", routes![admin_index])
+        .mount("/", routes![public::get_all_products])
+        .mount("/admin", routes![protected::get_admin])
 }
